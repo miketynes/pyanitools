62cdc12d585fcf6fa5b9c9ee52e4e3cacc49a46f
miketynes <mtynes@uchicago.edu>
2023-04-14 10:02:13 -0500

    move to just setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,24 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    install_requires = f.readlines()
 
 setup(
     name='pyanitools',
     version='0.0.1',
-    #install_requires=[],
+    packages=find_packages(include=['pyanitools']),
+    include_package_data=True,
+    description='File I/O for ani hdf5 files',
+    install_requires=install_requires,
+    python_requires=">=3.8.*",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Topic :: Scientific/Engineering"
+    ],
+    author="Roman Zubatyuk, Justin S. Smith, Michael Tynes",
+    url="https://github.com/miketynes/pyanitools"
 )
